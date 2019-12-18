ae128933e3e087e930f4b5ea4fc561887b39d8b5
Hellu Shiro! <katobmt107@gmail.com>
2019-12-18 14:09:45 +0700

    7

---
+++ caculator/src/app/progress-bar/progress-bar.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar',
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.css']
+})
+export class ProgressBarComponent implements OnInit {
+
+  @Input() backgroundColor = 'aqua';
+  @Input() progressColor = "green";
+  @Input() width = 50;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
